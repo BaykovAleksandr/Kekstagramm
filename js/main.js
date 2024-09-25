fdde5d2fd14d2a7dec85b2c103b2b7ff616bacc1
BaykovAleksandr <baykov-1988@mail.ru>
2024-09-25 22:50:36 +0300

    modified:   js/main.js modified:   js/util.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,36 @@
-import { getPictures } from './data.js';
 import { renderPictures } from './picture.js';
 import './form.js';
 
-renderPictures(getPictures());
+const getData = async (onSuccess, onFail) => {
+  try {
+    const response = await fetch(
+      'https://25.javascript.htmlacademy.pro/kekstagram/data'
+    );
+
+    if (!response.ok) {
+      throw new Error('Не удалось загрузить фотографии');
+    }
+
+    const offers = await response.json();
+    onSuccess(offers);
+  } catch (error) {
+    onFail(error.message);
+  }
+};
+
+getData().then((data) => {
+
+}).catch((error) => {
+
+});
+
+const onLoadSuccess = (data) => {
+  renderPictures(data);
+}
+
+const onLoadError = (error) => {
+    showAlert('Не удалось загрузить файл. Попробуйте ещё раз');
+  };
+
+
+getData(onLoadSuccess, onLoadError);
